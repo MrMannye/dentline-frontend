1c1f26762bc478e30c73918e75036b7c72fafbc6
MrMannye <superbrandon2018@gmail.com>
2024-09-27 12:48:46 -0600

    Add CardDate to the Page Dates

--- app/dates/page.tsx
+++ app/dates/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import CardDate from "@/src/modules/date/CardDate"
+
 export default function page() {
   return (
     <div className="flex-1 w-full">
-      
+      <CardDate></CardDate>
     </div>
   )
 }
