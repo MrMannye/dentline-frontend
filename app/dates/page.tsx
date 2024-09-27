a13b546f2f2b861f973551a326c8c8e4de1bd2c3
MrMannye <superbrandon2018@gmail.com>
2024-09-27 12:51:30 -0600

    Modify CardDate

--- app/dates/page.tsx
+++ app/dates/page.tsx
@@ -4,7 +4,7 @@ import CardDate from "@/src/modules/date/CardDate"
 
 export default function page() {
   return (
-    <div className="flex-1 w-full">
+    <div className="flex-1 w-full p-8">
       <CardDate></CardDate>
     </div>
   )
