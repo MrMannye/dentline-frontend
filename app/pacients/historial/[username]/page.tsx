6295059025fd50b935f6ac24fd8fdd5c55c9f0c5
Manu Aguilera <Miguel>
2024-09-28 23:18:14 -0600

    ESLint Configuration

--- app/pacients/historial/[username]/page.tsx
+++ app/pacients/historial/[username]/page.tsx
@@ -4,56 +4,54 @@ import React from 'react'
 
 import DeleteIcon from '@mui/icons-material/Delete';
 
-export default function Historia({params} : {params: {username: string}}) {
+export default function Historia({ params }: { params: { username: string } }) {
 
-    const dateToday = new Date();
+	return (
+		<div className='w-full flex-1'>
+			<div className='flex items-center justify-around w-full bg-secundary-color text-white p-2'>
+				<Link className='' href={`/pacients/${params.username}`}>
+					<h3 className='text-center'>PERFIL</h3>
+				</Link>
+				<h3 className='text-center'>DATOS</h3>
+				<h3 className='text-center'>HISTORIAL</h3>
+			</div>
 
-    return (
-        <div className='w-full flex-1'>
-            <div className='flex items-center justify-around w-full bg-secundary-color text-white p-2'>
-                <Link className='' href={`/pacients/${params.username}`}>
-                    <h3 className='text-center'>PERFIL</h3>
-                </Link>
-                <h3 className='text-center'>DATOS</h3>
-                <h3 className='text-center'>HISTORIAL</h3>
-            </div>
+			<div className='relative w-full bg-primary-color flex flex-col items-center text-white py-8'>
+				<Avatar alt="Image Avatar"
+					src={"/img/home_image.png"}
+					sx={{ width: 82, height: 82 }} />
+				<h2 className="text-2xl mt-2">{params.username}</h2>
+				<span className='text-sm'>Paciente desde 2012</span>
+			</div>
 
-            <div className='relative w-full bg-primary-color flex flex-col items-center text-white py-8'>
-                <Avatar alt="Image Avatar"
-                    src={"/img/home_image.png"}
-                    sx={{ width: 82, height: 82 }} />
-                <h2 className="text-2xl mt-2">{params.username}</h2>
-                <span className='text-sm'>Paciente desde 2012</span>
-            </div>
+			<div className='mt-8 mx-4 space-y-6'>
+				{[1, 2, 3, 4].map((option) => {
+					return (
+						<div className='rounded-xl flex flex-col' key={option}>
+							<span className='bg-acent-color text-white text-center rounded-t-xl'>{"Enero 12 de 2022 - 10:45 AM"}</span>
+							<div className='text-xs mx-4 my-2 text-gray-400 flex flex-col'>
+								<h2 className='uppercase font-bold text-gray-700 mb-1'>Procedimientos:</h2>
+								<span>15 - Endodoncia</span>
+								<span>27, 31 - Aplicacion de Amalgama</span>
+							</div>
+							<hr />
+							<div className='text-xs mx-4 my-2 flex flex-col space-y-[3px]'>
+								<h3 className='font-bold text-gray-700'>Costo: <span className='font-normal text-gray-400'>$2500.00</span></h3>
+								<h3 className='font-bold text-gray-700'>Fecha: <span className='font-normal text-gray-400'>12 de enero de 2022</span></h3>
+								<h3 className='font-bold text-gray-700'>Notas: <span className='font-normal text-gray-400'>
+									Por lo menos la mitad de la superficie del diente está destruida por la caries, y la pulpa
+									puede estar afectada.
+								</span>
+								</h3>
+							</div>
+							<div className='bg-primary-200 w-full flex flex-row-reverse rounded-b-xl p-1'>
+								<DeleteIcon className='text-primary-color mx-3 text-base' />
+							</div>
+						</div>
+					)
+				})}
+			</div>
 
-            <div className='mt-8 mx-4 space-y-6'>
-                {[1, 2, 3, 4].map((option) => {
-                    return (
-                        <div className='rounded-xl flex flex-col' key={option}>
-                            <span className='bg-acent-color text-white text-center rounded-t-xl'>{"Enero 12 de 2022 - 10:45 AM"}</span>
-                            <div className='text-xs mx-4 my-2 text-gray-400 flex flex-col'>
-                                <h2 className='uppercase font-bold text-gray-700 mb-1'>Procedimientos:</h2>
-                                <span>15 - Endodoncia</span>
-                                <span>27, 31 - Aplicacion de Amalgama</span>
-                            </div>
-                            <hr />
-                            <div className='text-xs mx-4 my-2 flex flex-col space-y-[3px]'>
-                                <h3 className='font-bold text-gray-700'>Costo: <span className='font-normal text-gray-400'>$2500.00</span></h3>
-                                <h3 className='font-bold text-gray-700'>Fecha: <span className='font-normal text-gray-400'>12 de enero de 2022</span></h3>
-                                <h3 className='font-bold text-gray-700'>Notas: <span className='font-normal text-gray-400'>
-                                        Por lo menos la mitad de la superficie del diente está destruida por la caries, y la pulpa 
-                                        puede estar afectada.
-                                    </span>
-                                </h3>
-                            </div>
-                            <div className='bg-primary-200 w-full flex flex-row-reverse rounded-b-xl p-1'>
-                                <DeleteIcon className='text-primary-color mx-3 text-base'/>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
-
-        </div>
-    )
+		</div>
+	)
 }
--- app/pacients/page.tsx
+++ app/pacients/page.tsx
@@ -5,47 +5,39 @@ import { Pacientes } from '../../src/utils/Mock';
 import Link from 'next/link';
 
 import Avatar from '../../src/utils/Avatar';
-import { Zoom, Fab } from '@mui/material';
+import { Fab } from '@mui/material';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import { useTheme } from '@mui/material/styles';
 
 export default function Pacients() {
 
-    const theme = useTheme();
+	const listArray: Array<string> = [];
+	const addtoArray = (letter: string): string => {
+		if (!listArray.includes(letter)) {
+			listArray.push(letter);
+			return letter;
+		} else {
+			return "";
+		}
+	}
 
-    const listArray: Array<String> = [];
-    const addtoArray = (letter: string): string => {
-        if (!listArray.includes(letter)) {
-            listArray.push(letter);
-            return letter;
-        } else {
-            return "";
-        }
-    }
-
-    const transitionDuration = {
-        enter: theme.transitions.duration.enteringScreen,
-        exit: theme.transitions.duration.leavingScreen,
-    };
-
-    return (
-        <div className='px-4 space-y-4 w-full flex-1'>
-            {Pacientes.map((paciente) => {
-                return (
-                    <div key={paciente.id} className="flex items-center space-x-4">
-                        <span className='text-3xl font-normal text-acent-color w-5'>{addtoArray(paciente.nombre.charAt(0))}</span>
-                        <Link href={`/pacients/${paciente.nombre}`} className="flex items-center space-x-4">
-                            <Avatar image={paciente.img} />
-                            <span className='text-base'>{paciente.nombre}</span>
-                        </Link>
-                    </div>
-                )
-            })}
-            <div className='fixed right-7 bottom-32'>
-                <Fab aria-label="add" size='large' color='primary'>
-                    <PersonAddIcon className='text-white' />
-                </Fab>
-            </div>
-        </div>
-    )   
+	return (
+		<div className='px-4 space-y-4 w-full flex-1'>
+			{Pacientes.map((paciente) => {
+				return (
+					<div key={paciente.id} className="flex items-center space-x-4">
+						<span className='text-3xl font-normal text-acent-color w-5'>{addtoArray(paciente.nombre.charAt(0))}</span>
+						<Link href={`/pacients/${paciente.nombre}`} className="flex items-center space-x-4">
+							<Avatar image={paciente.img} />
+							<span className='text-base'>{paciente.nombre}</span>
+						</Link>
+					</div>
+				)
+			})}
+			<div className='fixed right-7 bottom-32'>
+				<Fab aria-label="add" size='large' color='primary'>
+					<PersonAddIcon className='text-white' />
+				</Fab>
+			</div>
+		</div>
+	)
 }
