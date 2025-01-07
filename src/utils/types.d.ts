4901ee2268c8f0605d77861cfd868daccdf77dad
Manu Aguilera <Miguel>
2025-01-06 23:17:48 -0600

    Feauture: Implement API BBVA for QR CoDi

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -1,3 +1,18 @@
 export interface DataPacient {
 	username: string;
 }
+
+interface HistorialCita {
+	alergias: string;           // Alergias del paciente
+	costoTotal: number;         // Costo total de la cita (en número)
+	edadPaciente: number;       // Edad del paciente
+	fecha: string;              // Fecha de la cita (puede ser un timestamp o un string legible)
+	idPaciente: number;         // ID del paciente (entero)
+	motivo: string;             // Motivo de la cita
+	nombreDentista: string;     // Nombre del dentista asignado
+	nombrePaciente: string;     // Nombre del paciente
+	observaciones: string;      // Observaciones de la cita
+	profesionPaciente: string;  // Profesión del paciente
+	telefonoDentista: string;   // Teléfono de contacto del dentista
+	tipoSangre: string;         // Tipo de sangre del paciente
+}
